3164e8dd944938895692d7c9a377657bb6760366
marazzax <77912698+marazzax@users.noreply.github.com>
2022-10-31 06:25:38 +0530

    Homepage build

---
+++ src/components/Product.js
@@ -0,0 +1,54 @@
+import Image from "next/image";
+import { useState } from "react";
+import { StarIcon } from "@heroicons/react/24/solid";
+import Currency from "react-currency-formatter";
+
+const MAX_RATING = 5;
+const MIN_RATING = 1;
+
+function Product({ id, title, price, description, category, image }) {
+  //below we have a piece of state which is like short-term memory for the random generation of product stars with a small math logic
+  const [rating] = useState(
+    Math.floor(Math.random() * (MAX_RATING - MIN_RATING + 1)) + MIN_RATING
+  );
+
+  //Has prime or not random generator
+  const [hasPrime] = useState(Math.random() < 0.5);
+
+  return (
+    <div className="relative flex flex-col m-5 bg-white z-30 p-10">
+      <p className="absolute top-2 right-2 text-xs italic text-gray-400">
+        {category}
+      </p>
+
+      <Image src={image} height={200} width={200} objectFit="contain" />
+
+      <h4 className="my-3">{title}</h4>
+
+      <div className="flex">
+        {Array(rating)
+          .fill()
+          .map((_, i) => (
+            <StarIcon className="h-5 text-yellow-500" />
+          ))}
+      </div>
+
+      <p className="text-xs my-2 line-clamp-2">{description}</p>
+
+      <div className="mb-5">
+        <Currency quantity={price} currency="INR" />
+      </div>
+
+      {hasPrime && (
+        <div className="flex items-center space-x-2 -mt-5">
+          <img className="w-12" src="https://links.papareact.com/fdw" alt="" />
+          <p className="text-xs text-gray-500">FREE Next-Day Delivery</p>
+        </div>
+      )}
+
+      <button className="mt-auto button ">Add to Basket</button>
+    </div>
+  );
+}
+
+export default Product;
---
+++ src/pages/index.js
@@ -0,0 +1,37 @@
+import Head from "next/head";
+import Banner from "../components/Banner";
+import Header from "../components/Header";
+import ProductFeed from "../components/ProductFeed";
+
+export default function Home({ products }) {
+  return (
+    <div className="bg-gray-100  ">
+      <Head>
+        <title>Amazon</title>
+      </Head>
+
+      {/* Header */}
+      <Header />
+
+      <main className="max-w-screen-2xl mx-auto">
+        {/* Banner */}
+        <Banner />
+
+        {/* ProductFeed */}
+        <ProductFeed products={products} />
+      </main>
+    </div>
+  );
+}
+
+export async function getServerSideProps(context) {
+  const products = await fetch("http://fakestoreapi.com/products").then((res) =>
+    res.json()
+  );
+
+  return {
+    props: {
+      products,
+    },
+  };
+}
